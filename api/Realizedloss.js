a8708700b1b4841ec35cb307cff4974eb318c6f7
sagecrypto93 <sage.crypto93@outlook.com>
2025-07-07 12:51:45 +0100

    Update and rename market_value_to_realized_value.js to Realizedloss.js

--- api/Realizedloss.js
+++ api/Realizedloss.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   const token = "0d866412-d29c-41ed-a044-7be4fb52478a"; 
-  const endpoint = `https://api.researchbitcoin.net/v1/market_value_to_realized_value/mvrv?token=${token}&date_field=2011-01-01&output_format=json`;
-  //CHANGE NAME AND 2NDNAME to API names
+  const endpoint = `https://api.researchbitcoin.net/v1/realizedloss/realized_loss?token=${token}&date_field=2011-01-01&output_format=json`;
+  // Change title to CATEGORY name
+  //Change NAME to CATEGORY name and 2NDNAME to data_field name
 
   try {
     const response = await fetch(endpoint);
